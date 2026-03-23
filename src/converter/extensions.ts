ec615d81f500aa17d51e55562158c8b52cafa79a
Alex Lipovka <alex.lipovka@gmail.com>
2026-03-23 18:33:34 +0400

    Use standart hl7 extension for columnWidth

--- src/converter/extensions.ts
+++ src/converter/extensions.ts
@@ -59,7 +59,7 @@ export enum ExtensionIdentifier {
     MDEditorFeature = 'https://emr-core.beda.software/StructureDefinition/mdEditorFeature',
     ChartYAxisRange = 'https://emr-core.beda.software/StructureDefinition/chartYAxisRange',
     ChartHighlight = 'https://emr-core.beda.software/StructureDefinition/chartHighlight',
-    CoolumnWidth = 'https://emr-core.beda.software/StructureDefinition/columnWidth',
+    CoolumnWidth = 'http://hl7.org/fhir/uv/sdc/StructureDefinition/sdc-questionnaire-width',
 }
 
 export type ExtensionTransformer = {
@@ -514,7 +514,7 @@ export const extensionTransformers: ExtensionTransformer = {
         },
     },
     [ExtensionIdentifier.CoolumnWidth]: {
-        path: { extension: 'valueInteger', questionnaire: 'columnWidth' },
+        path: { extension: 'valueQuantity', questionnaire: 'columnWidth' },
     },
 };
 
