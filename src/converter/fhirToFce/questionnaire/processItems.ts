278914dc2abccb2ca18ebcba991dc010c01ba597
Dmitry Shutov <dmitry.s@beda.software>
2026-08-10 14:01:57 +0500

    Add withTranslations flag to toFirstClassExtension.
    Skip converting translation extensions to FCE by default so they stay in FHIR format unless callers opt in.

--- src/converter/fhirToFce/questionnaire/processItems.ts
+++ src/converter/fhirToFce/questionnaire/processItems.ts
@@ -3,16 +3,19 @@ import { Questionnaire as FHIRQuestionnaire, QuestionnaireItem as FHIRQuestionna
 import { processExtensibleElement, processPrimitiveExtensions } from '../utils';
 import { FCEQuestionnaireItem } from '../../../fce.types';
 
-export function processItems(fhirQuestionnaire: FHIRQuestionnaire) {
-    return fhirQuestionnaire.item?.map((item) => convertItemProperties(item));
+export function processItems(fhirQuestionnaire: FHIRQuestionnaire, withTranslations = false) {
+    return fhirQuestionnaire.item?.map((item) => convertItemProperties(item, withTranslations));
 }
 
-function convertItemProperties(item: FHIRQuestionnaireItem): FCEQuestionnaireItem {
+function convertItemProperties(item: FHIRQuestionnaireItem, withTranslations = false): FCEQuestionnaireItem {
     const { item: nestedItems, ...rest } = item;
-    const newItem = processExtensibleElement(processPrimitiveExtensions(rest)) as FCEQuestionnaireItem;
+    const newItem = processExtensibleElement(
+        processPrimitiveExtensions(rest, withTranslations),
+        withTranslations,
+    ) as FCEQuestionnaireItem;
 
     if (nestedItems) {
-        newItem.item = nestedItems.map((nestedItem) => convertItemProperties(nestedItem));
+        newItem.item = nestedItems.map((nestedItem) => convertItemProperties(nestedItem, withTranslations));
     }
 
     return newItem;
